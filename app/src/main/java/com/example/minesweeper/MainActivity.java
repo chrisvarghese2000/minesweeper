6a055f70e172edd20dd095d87f7d55cc7f4d1e95
abhi <gaddipatia@gmail.com>
2020-05-05 20:36:13 -0400

    abhi pt2

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +18,15 @@ public class MainActivity extends Activity {
 //        startActivity(intent);
         setContentView(R.layout.activity_main);
         GameEngine.getInstance().createGrid(this);
+        ImageButton btn = findViewById(R.id.imageButton);
+        if(GameEngine.getInstance().gameState.equals(GameEngine.WON)) {
+            btn.setImageResource(R.drawable.wink);
+        }
+        if(GameEngine.getInstance().gameState.equals(GameEngine.LOST)) {
+            btn.setImageResource(R.drawable.sad);
+        }
+        if(GameEngine.getInstance().gameState.equals(GameEngine.PLAYING)) {
+            btn.setImageResource(R.drawable.smile2);
+        }
     }
 }
